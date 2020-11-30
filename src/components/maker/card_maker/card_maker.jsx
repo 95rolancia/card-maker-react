7385ff479ead9fa64adc7115cff4614fe8b2963f
95rolancia <95jjangjun@gmail.com>
2020-11-30 20:14:07 +0900

    add cardMaker and cardPreview layout

---
+++ src/components/maker/card_maker/card_maker.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import styles from "./card_maker.module.css";
+const CardMaker = (props) => {
+  return (
+    <section className={styles.cardMaker}>
+      <h1 className={styles.title}>Card Maker</h1>
+    </section>
+  );
+};
+
+export default CardMaker;
--- src/components/maker/maker.jsx
+++ src/components/maker/maker.jsx
@@ -3,6 +3,8 @@ import styles from "./maker.module.css";
 import Header from "../header/header";
 import Footer from "../footer/footer";
 import { useHistory } from "react-router-dom";
+import CardMaker from "./card_maker/card_maker";
+import CardPreview from "./card_preview/card_preview";
 
 const Maker = ({ authService }) => {
   const history = useHistory();
@@ -19,7 +21,11 @@ const Maker = ({ authService }) => {
   });
   return (
     <section className={styles.maker}>
-      <Header onLogout={onLogout} />
+      <Header className={styles.header} onLogout={onLogout} />
+      <section className={styles.content}>
+        <CardMaker />
+        <CardPreview />
+      </section>
       <Footer />
     </section>
   );
