84499bb67a6d2e32afbfca360c342011f320b429
95rolancia <95jjangjun@gmail.com>
2020-12-04 23:03:31 +0900

    add CardAddForm component

--- src/components/card_preview/card_preview.jsx
+++ src/components/card_preview/card_preview.jsx
@@ -1,9 +1,15 @@
 import React from "react";
+import Card from "../card/card";
 import styles from "./card_preview.module.css";
-const CardPreview = (props) => {
+const CardPreview = ({ cards }) => {
   return (
     <section className={styles.cardPreview}>
       <h1 className={styles.title}>Card Preview</h1>
+      <ul className={styles.cards}>
+        {cards.map((card) => (
+          <Card card={card} />
+        ))}
+      </ul>
     </section>
   );
 };
--- src/components/maker/maker.jsx
+++ src/components/maker/maker.jsx
@@ -1,12 +1,47 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import styles from "./maker.module.css";
 import Header from "../header/header";
 import Footer from "../footer/footer";
 import { useHistory } from "react-router-dom";
-import CardMaker from "./card_maker/card_maker";
-import CardPreview from "./card_preview/card_preview";
+import CardMaker from "../card_maker/card_maker";
+import CardPreview from "../card_preview/card_preview";
 
 const Maker = ({ authService }) => {
+  const [cards, setCards] = useState([
+    {
+      id: "1",
+      name: "rolancia",
+      company: "pnu",
+      theme: "Dark",
+      title: "FE develpoer",
+      email: "95jjangjun@gmail.com",
+      message: "go for it",
+      fileName: "rolancia",
+      fileURL: null,
+    },
+    {
+      id: "2",
+      name: "rolancia",
+      company: "pnu",
+      theme: "Light",
+      title: "FE develpoer",
+      email: "95jjangjun@gmail.com",
+      message: "go for it",
+      fileName: "rolancia",
+      fileURL: null,
+    },
+    {
+      id: "3",
+      name: "rolancia",
+      company: "pnu",
+      theme: "Colorful",
+      title: "FE develpoer",
+      email: "95jjangjun@gmail.com",
+      message: "go for it",
+      fileName: "rolancia",
+      fileURL: "rolancia.png",
+    },
+  ]);
   const history = useHistory();
   const onLogout = () => {
     authService.logout();
@@ -19,12 +54,18 @@ const Maker = ({ authService }) => {
       }
     });
   });
+
+  const addCard = (card) => {
+    const updated = [...cards, card];
+    setCards(updated);
+  };
+
   return (
     <section className={styles.maker}>
       <Header className={styles.header} onLogout={onLogout} />
       <section className={styles.content}>
-        <CardMaker />
-        <CardPreview />
+        <CardMaker cards={cards} addCard={addCard} />
+        <CardPreview cards={cards} />
       </section>
       <Footer />
     </section>
