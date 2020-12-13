73f5056137dd1299054805845ef036a34884058b
95rolancia <95jjangjun@gmail.com>
2020-12-13 19:42:12 +0900

    add loading spinner for file input

--- src/service/image_uploader.js
+++ src/service/image_uploader.js
@@ -2,9 +2,12 @@ class ImageUploader {
   async upload(file) {
     const data = new FormData();
     data.append("file", file);
-    data.append("upload_preset", "hcxjbrtt");
+    data.append(
+      "upload_preset",
+      process.env.REACT_APP_CLOUDINARY_UPLOAD_PRESET
+    );
     const res = await fetch(
-      "https://api.cloudinary.com/v1_1/dqcxmcxro/image/upload",
+      `https://api.cloudinary.com/v1_1/${process.env.REACT_APP_CLOUDINARY_CLOUD_NAME}/image/upload`,
       {
         method: "POST",
         body: data,
