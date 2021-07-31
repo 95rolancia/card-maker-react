c2a1d3cdfb4d75a6b362e214bdfab8cb1281e7cd
rolancia <95jjangjun@gmail.com>
2021-07-31 20:27:06 +0900

    refactor: use env in image_uploader.js

--- src/service/image_uploader.js
+++ src/service/image_uploader.js
@@ -1,15 +1,14 @@
 class ImageUploader {
   async upload(file) {
+    const url = `https://api.cloudinary.com/v1_1/${process.env.REACT_APP_CLOUDINARY_CLOUD_NAME}/image/upload`;
     const data = new FormData();
+
     data.append('file', file);
     data.append('upload_preset', process.env.REACT_APP_CLOUDINARY_UPLOAD_PRESET);
-    const res = await fetch(
-      `https://api.cloudinary.com/v1_1/${process.env.REACT_APP_CLOUDINARY_CLOUD_NAME}/image/upload`,
-      {
-        method: 'POST',
-        body: data,
-      }
-    );
+    const res = await fetch(url, {
+      method: 'POST',
+      body: data,
+    });
     return await res.json();
   }
 }
